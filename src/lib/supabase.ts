363f013ecfa61565f51bc190f1e917b434f34123
holy-cursor <d.adeyemi.csc2023025@student.oauife.edu.ng>
2025-11-27 02:49:57 +0100

    Add environment variables for Supabase credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://exrojkbrugojwynbvwco.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4cm9qa2JydWdvand5bmJ2d2NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxOTg4NTAsImV4cCI6MjA3OTc3NDg1MH0.ixGnOtca7hCuoHl6NFej8CVDiEDHpHtmPQ0UHAsJ86o";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
